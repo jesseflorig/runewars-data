a608ad306de344f9dbfb797d89a64a0aa82f2b11
Jesse Florig <jesseflorig@gmail.com>
2017-07-25 15:37:08 -0400

    Adding units WIP

--- data/units.js
+++ data/units.js
@@ -1,3 +1,69 @@
 [
-  
+  {
+    name: "Kari Wraithstalker",
+    faction: "Daqan",
+    unique: true,
+    type: "infantry",
+    setupAbilities: [
+      {
+        name: "Aymhelin Allies",
+        text: "Your army can include one non-unique infantry unit from the Latari Elves."
+      }
+    ]
+    activatedAbilities: [
+      {
+        type: "attack",
+        cost: "surge"
+        costCount: "+"
+        text: "Chose another enemey unit at range 1-5. That enemy suffers 2 damage per [SURGE] spent"
+      }
+    ],
+    passiveAbilities: [
+      {
+        type: "brutal",
+        amount: 1
+      },
+      {
+        type: "precise",
+        amount: 1
+      }
+    ]
+    attacks: [
+      {
+        type: "range",
+        white: 1,
+        blue: 1
+      },
+      {
+        type: "melee",
+        white: 1,
+        blue: 1
+      }
+    ],
+    formations: [
+      {
+        trays: 1,
+        rows: 1,
+        ranks: 1,
+        cost: 32,
+        availableUpgrades: [
+          "artifact",
+          "unique"
+        ]
+      }
+    ]
+    commandWheel: {
+      actions: [
+        {
+          initiative: 3
+          type: "march",
+          color: "blue",
+          speed: "2"
+        }
+      ],
+      modifiers: [
+
+      ]
+    }
+  }
 ]
