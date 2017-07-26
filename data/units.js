330832ac09babebd0a6e68d6529f845e4274eba8
Jesse Florig <jesseflorig@gmail.com>
2017-07-25 23:58:42 -0400

    Added Lord Hawthorne

--- data/units.js
+++ data/units.js
@@ -15,7 +15,7 @@
     activatedAbilities: [
       {
         type: "attack",
-        cost: "surge+"
+        cost: "surge+",
         text: "Chose another enemey unit at range 1-5. That enemy suffers 2 damage per [SURGE] spent"
       }
     ],
@@ -56,40 +56,40 @@
     commandWheel: {
       actions: [
         {
-          initiative: 3
+          initiative: 3,
           type: "march",
           color: "blue",
           speed: 2
         },
         {
-          initiative: 4
+          initiative: 4,
           type: "march",
           color: "blue",
           speed: 3
         },
         {
-          initiative: 2
+          initiative: 2,
           type: "rotate",
           color: "green",
         },
         {
-          initiative: 2
+          initiative: 2,
           type: "special",
           color: "green",
         },
         {
-          initiative: 2
+          initiative: 2,
           type: "ranged",
           color: "green",
         },
         {
-          initiative: 5
+          initiative: 5,
           type: "shift",
           color: "green",
           speed: 1
         },
         {
-          initiative: 4
+          initiative: 4,
           type: "melee",
           color: "red"
         }
@@ -131,6 +131,134 @@
         {
           type: "rally",
           color: "white"
+        }
+      ]
+    }
+  },
+  {
+    name: "Lord Hawthorne",
+    faction: "Daqan",
+    unique: true,
+    defense: 3,
+    health: 4,
+    type: "cavalry",
+    setupAbilities: [
+      {
+        name: "Lessons of Seragart",
+        text: "You may reconfigure your trays in each of your units before setup. No units can be more than 4 trays wide or 4 trays deep after reconfiguring, and each rank must be a full rank."
+      }
+    ]
+    activatedAbilities: [
+      {
+        type: "attack",
+        cost: "surge2",
+        text: "After you perform a [MELEE] you may perform a [MELEE] against another enemy that has not been targeted by your [MELEE] durnig this activation."
+      },
+      {
+        type: "special",
+        text: "Each other ally at range 1-3 receives 1 inspiration token"
+      }
+    ],
+    passiveAbilities: [
+      {
+        type: "brutal",
+        amount: 1
+      }
+    ]
+    attacks: [
+      {
+        type: "melee",
+        white: 1,
+        red: 1
+      }
+    ],
+    formations: [
+      {
+        trays: 1,
+        rows: 1,
+        ranks: 1,
+        cost: 34,
+        availableUpgrades: [
+          "artifact",
+          "unique"
+        ]
+      }
+    ]
+    commandWheel: {
+      actions: [
+        {
+          initiative: 3,
+          type: "march",
+          color: "blue",
+          speed: 2
+        },
+        {
+          initiative: 4,
+          type: "march",
+          color: "blue",
+          speed: 3
+        },
+        {
+          initiative: 6,
+          type: "march",
+          color: "blue",
+          speed: 4
+        },
+        {
+          initiative: 2,
+          type: "rotate",
+          color: "green",
+        },
+        {
+          initiative: 5,
+          type: "shift",
+          color: "green",
+          speed: 1
+        },
+        {
+          initiative: 6,
+          type: "melee",
+          color: "red"
+        },
+        {
+          initiative: 2,
+          type: "special",
+          color: "yellow"
+        },
+        {
+          initiative: 2,
+          type: "melee",
+          color: "yellow"
+        }
+      ],
+      modifiers: [
+        {
+          type: "bank",
+          color: "blue",
+          mod: -1
+        },
+        {
+          type: "charge",
+          color: "blue"
+        },
+        {
+          type: "turnCharge",
+          color: "blue"
+          mod: -1
+        },
+        {
+          type: "hit",
+          color: "red"
+          mod: 1
+        },
+        {
+          type: "rally",
+          color: "white"
+        },
+        {
+          type: "defense",
+          color: "white",
+          mod: 1
         }
       ]
     }
